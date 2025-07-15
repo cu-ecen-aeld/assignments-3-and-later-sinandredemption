cca4442c0374b7a39ef209a7c41cc6d833370054
fahad <fahad@Ubuntu20.04.myguest.virtualbox.org>
2025-07-16 00:33:04 +0530

    assignment 5 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -17,10 +17,11 @@
 
 int sockfd = 0, remote_fd = 0;
 FILE *fptr = NULL, *fptr2 = NULL;
+char *packet = NULL;
 
 void sighandler(int sig)
 {
-    puts("Exiting gracefully...");
+    syslog(LOG_DEBUG, "Exiting gracefully...");
     syslog(LOG_DEBUG, "Caught signal, exiting");
 
     if (sockfd != 0)
@@ -33,6 +34,9 @@ void sighandler(int sig)
     }
     if (fptr2)
         fclose(fptr2);
+
+    if (packet)
+        free(packet);
 
     exit(EXIT_SUCCESS);
 }
@@ -56,7 +60,7 @@ int main(int argc, char **argv)
         exit(__LINE__);
     }
 
-    puts("Opening file /var/tmp/aesdsocketdata...");
+    syslog(LOG_DEBUG, "Opening file /var/tmp/aesdsocketdata...");
     fptr = fopen("/var/tmp/aesdsocketdata", "ab+");
     fptr2 = fopen("/var/tmp/aesdsocketdata", "rb");
 
@@ -70,14 +74,14 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    puts("Running getaddrinfo()...");
+    syslog(LOG_DEBUG, "Running getaddrinfo()...");
     rv = getaddrinfo(NULL, MYPORT, &hints, &res);
     if (rv != 0) {
         printf("[ERROR] getaddrinfo returned error code: %d", rv);
         exit(__LINE__);
     }
 
-    puts("Running socket()...");
+    syslog(LOG_DEBUG, "Running socket()...");
     if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
     {
         perror("socket");
@@ -88,7 +92,7 @@ int main(int argc, char **argv)
 
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
 
-    puts("Running bind()...");
+    syslog(LOG_DEBUG, "Running bind()...");
     if ((bind(sockfd, res->ai_addr, res->ai_addrlen)) == -1)
     {
         close(sockfd);
@@ -97,23 +101,24 @@ int main(int argc, char **argv)
     }
 
     if (argc > 1) {
-        puts("Running fork()...");
+        syslog(LOG_DEBUG, "Running fork()...");
         int p = fork();
         if (p != 0) {
-            puts("Goodbye!");
+            syslog(LOG_DEBUG, "Goodbye!");
             exit(EXIT_SUCCESS);
         }
     }
 
-    puts("Running listen()");
+    syslog(LOG_DEBUG, "Running listen()");
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
         exit(__LINE__);
     }
+    syslog(LOG_DEBUG, "Listening sucessful.");
 
     addr_size = sizeof(their_addr);
     // Connect to client
-    // puts("Connecting to client");
+    // syslog(LOG_DEBUG, "Connecting to client");
     // if (connect(sockfd, &their_addr, INET_ADDRSTRLEN) == -1) {
     //     perror("connect");
     //     exit(1);
@@ -122,7 +127,17 @@ int main(int argc, char **argv)
     
 
     for (;;) {
+        syslog(LOG_DEBUG, "Attempting to accept...");
         remote_fd = accept(sockfd, &their_addr, &addr_size);
+
+        if (remote_fd == -1) {
+            perror("accept");
+            // If accept fails, it's usually a recoverable error (e.g., EINTR), so continue to accept.
+            // However, a persistent error might indicate a bigger problem.
+            continue; // Go back and try to accept again
+        }
+
+        printf("Recieved fd: %d\n", remote_fd);
 
         inet_ntop(AF_INET, &(((struct sockaddr_in*)&their_addr)->sin_addr), their_addr_str, INET_ADDRSTRLEN);
         syslog(LOG_DEBUG, "Accepting connection from %s", their_addr_str);
@@ -130,7 +145,7 @@ int main(int argc, char **argv)
         for (;;)
         {
             // Recieve data
-            char buf[MAX_DATA_SIZE], *packet = malloc(MAX_DATA_SIZE);
+            char buf[MAX_DATA_SIZE];
             int numbytes, packet_size = 0;
             bool packet_recieved = false;
 
@@ -145,25 +160,23 @@ int main(int argc, char **argv)
                 }
 
                 if (numbytes == 0) {
-                    puts("Connection closed by remote");
+                    syslog(LOG_DEBUG, "Connection closed by remote");
                     break;
                 }
 
-                buf[numbytes + 1] = '\0';
-
-                packet = realloc(packet, packet_size + numbytes);
+                packet = realloc(packet, packet_size + numbytes + 1);
                 if (packet == NULL) {
             fprintf(stderr, "realloc failed with errno: %d\n", errno); // Add this line
                     perror("realloc");
                     exit(__LINE__);
                 }
 
-                strcpy(packet + packet_size, buf);
+                memcpy(packet + packet_size, buf, numbytes);
 
                 packet_size += numbytes;
 
                 // Check if packet is complete
-                for (char *c = packet; *c; c++)
+                for (char *c = packet; c != packet + packet_size; c++)
                 {
                     if (*c == '\n') packet_recieved = true;
                 }
@@ -183,23 +196,28 @@ int main(int argc, char **argv)
 
             fseek(fptr2, 0, SEEK_SET);
             // Transmit back
-            for (;;)
+            for (;numbytes > 0;)
             {
+                syslog(LOG_DEBUG, "Transmitting back...");
                 size_t bread = fread(buf, sizeof(char), MAX_DATA_SIZE, fptr2);
 
                 // Transmit
-                if (send(remote_fd, buf, bread, 0) == -1) {
+                if ((bread > 0) && (send(remote_fd, buf, bread, 0) == -1)) {
                     perror("send");
                     exit(__LINE__);
                 } else {
                     printf("Sent %d data bytes to %s", bread, their_addr_str);
                 }
 
-                if (bread != MAX_DATA_SIZE) break; // Tx complete
+                if (bread != MAX_DATA_SIZE) {
+                    syslog(LOG_DEBUG, "Transmission complete");
+                    break; // Tx complete
+                }
             }
 
             if (numbytes == 0) {
                 free(packet);
+                packet = NULL;
                 syslog(LOG_DEBUG, "Closed connection from %s", their_addr_str);
                 break;
             }
@@ -209,6 +227,8 @@ int main(int argc, char **argv)
     close(sockfd);
     fclose(fptr);
     fclose(fptr2);
+
+    syslog(LOG_DEBUG, "Shouldn't reach here");
 
     return 0;
 }
