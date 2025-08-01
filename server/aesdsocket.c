4dd2c12d51cae747f9194c2473d2fcd278c9d120
fahad <fahad@Ubuntu20.04.myguest.virtualbox.org>
2025-08-02 01:21:57 +0530

    assignement 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,17 +7,37 @@
 #include <signal.h>
 #include <stdio.h>
 #include <sys/syslog.h>
+#include <sys/time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include "queue.h"
 
 #define MYPORT "9000"
 #define BACKLOG 10
 #define MAX_DATA_SIZE 1024
 
-int sockfd = 0, remote_fd = 0;
-FILE *fptr = NULL, *fptr2 = NULL;
-char *packet = NULL;
+struct conn_thread_args_s
+{
+    int clientfd;
+    bool status_flag;
+};
+
+struct thread_list_s
+{
+    pthread_t thread_id;
+    struct conn_thread_args_s conn_thread_args;
+    SLIST_ENTRY(thread_list_s) entries;
+};
+
+typedef struct thread_list_s thread_list_t;
+
+int sockfd = 0;
+FILE* fptr_w = NULL;
+pthread_mutex_t data_mutex;
+SLIST_HEAD(slisthead, thread_list_s) head;
 
 void sighandler(int sig)
 {
@@ -26,19 +46,153 @@ void sighandler(int sig)
 
     if (sockfd != 0)
         close(sockfd);
-    if (remote_fd != 0)
-        close(remote_fd);
-    if (fptr) {
-        fclose(fptr);
+   
+    // Wait for all threads to join
+    struct thread_list_s *thread, *tp;
+    SLIST_FOREACH_SAFE(thread, &head, entries, tp) {
+        printf("Joining thread %d\n", (int)(thread->thread_id));
+        // Join
+        while (true) {
+            if (thread->conn_thread_args.status_flag == true)
+            {
+                if (pthread_join(thread->thread_id, NULL) != 0) {
+                    perror("pthread_join");
+                    exit(__LINE__);
+                }
+                break;
+            }
+        }
+
+        // Remove 
+        SLIST_REMOVE(&head, thread, thread_list_s, entries);
+        free(thread);
+    }
+
+    if (fptr_w) {
+        fclose(fptr_w);
         remove("/var/tmp/aesdsocketdata");
     }
-    if (fptr2)
-        fclose(fptr2);
 
-    if (packet)
-        free(packet);
+    pthread_mutex_lock(&data_mutex);
+    pthread_mutex_unlock(&data_mutex);
+    pthread_mutex_destroy(&data_mutex);
 
     exit(EXIT_SUCCESS);
+}
+
+void timer_handler(int signo) {
+    if (signo != SIGALRM) {
+        perror("signo");
+        exit(__LINE__);
+    }
+
+    char rfc2822time[128], s[128+14];
+    pthread_mutex_lock(&data_mutex);
+
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+
+    strftime(rfc2822time, sizeof(rfc2822time), "%a, %d %b %Y %H:%M:%S %z", tm_info);
+
+    sprintf(s, "timestamp:%s\n", rfc2822time);
+
+    fwrite(s, sizeof(char), strlen(s), fptr_w);
+    fflush(fptr_w);
+
+    pthread_mutex_unlock(&data_mutex);
+}
+
+void* process_connection(void* args)
+{
+    struct conn_thread_args_s *conn_thread_args = (struct conn_thread_args_s*)args;
+    char *packet = NULL;
+    FILE *fptr_r = fopen("/var/tmp/aesdsocketdata", "rb");
+    if (fptr_r == NULL) {
+        perror("fopen");
+        exit(__LINE__);
+    }
+    printf("Entering new thread with clientfd: %d\n", conn_thread_args->clientfd);
+    for (;;)
+    {
+        // Recieve data
+        char buf[MAX_DATA_SIZE];
+        int numbytes, packet_size = 0;
+        bool packet_recieved = false;
+
+        while (!packet_recieved) {
+            numbytes = recv(conn_thread_args->clientfd, buf, MAX_DATA_SIZE - 1, 0);
+            if (numbytes == -1) {
+                perror("recv");
+                exit(__LINE__);
+            }
+
+            if (numbytes == 0) {
+                pthread_mutex_lock(&data_mutex);
+                syslog(LOG_DEBUG, "Connection closed by remote");
+                pthread_mutex_unlock(&data_mutex);
+                break;
+            }
+
+            packet = realloc(packet, packet_size + numbytes + 1);
+            if (packet == NULL) {
+                perror("realloc");
+                exit(__LINE__);
+            }
+
+            memcpy(packet + packet_size, buf, numbytes);
+
+            packet_size += numbytes;
+
+            // Check if packet is complete
+            for (char *c = packet; c != packet + packet_size; c++)
+            {
+                if (*c == '\n') packet_recieved = true;
+            }
+        }
+
+        pthread_mutex_lock(&data_mutex);
+
+        if (numbytes > 0) {
+            // Packet is recieved
+            // So dump
+            size_t bwritten = 0;
+            bwritten = fwrite(packet, sizeof(char), packet_size, fptr_w);
+            if (bwritten != packet_size) {
+                perror("fwrite");
+                exit(__LINE__);
+            }
+            fflush(fptr_w);
+        }
+
+        fseek(fptr_r, 0, SEEK_SET);
+        // Transmit back
+        for (;numbytes > 0;)
+        {
+            syslog(LOG_DEBUG, "Transmitting back...");
+            size_t bread = fread(buf, sizeof(char), MAX_DATA_SIZE, fptr_r);
+
+            // Transmit
+            if ((bread > 0) && (send(conn_thread_args->clientfd, buf, bread, 0) == -1)) {
+                perror("send");
+                exit(__LINE__);
+            }
+
+            if (bread != MAX_DATA_SIZE) {
+                syslog(LOG_DEBUG, "Transmission complete");
+                break; // Tx complete
+            }
+        }
+
+        pthread_mutex_unlock(&data_mutex);
+        if (numbytes == 0) {
+            free(packet);
+            //syslog(LOG_DEBUG, "Closed connection from %s", their_addr_str);
+            break;
+        }
+    }
+    close(conn_thread_args->clientfd);
+    fclose(fptr_r);
+    conn_thread_args->status_flag = true; // signal completion to parent
 }
 
 int main(int argc, char **argv)
@@ -48,6 +202,11 @@ int main(int argc, char **argv)
     struct addrinfo hints, *res;
     int rv;
     char their_addr_str[INET_ADDRSTRLEN];
+    struct itimerval new_value, old_value;
+
+    SLIST_INIT(&head);
+
+    pthread_mutex_init(&data_mutex, NULL);
 
     if (signal(SIGINT, sighandler) == SIG_ERR)
     {
@@ -59,15 +218,27 @@ int main(int argc, char **argv)
         perror("sigterm");
         exit(__LINE__);
     }
+    if (signal(SIGALRM, timer_handler) == SIG_ERR)
+    {
+        perror("sigalrm");
+        exit(__LINE__);
+    }
 
     syslog(LOG_DEBUG, "Opening file /var/tmp/aesdsocketdata...");
-    fptr = fopen("/var/tmp/aesdsocketdata", "ab+");
-    fptr2 = fopen("/var/tmp/aesdsocketdata", "rb");
-
-    if (fptr == NULL) {
+    
+    fptr_w = fopen("/var/tmp/aesdsocketdata", "ab+");
+    if (fptr_w == NULL) {
         perror("fopen");
         exit(__LINE__);
     }
+
+    struct itimerval tv;
+    tv.it_value.tv_sec = 10;
+    tv.it_value.tv_usec = 0;
+    tv.it_interval.tv_sec = 10;
+    tv.it_interval.tv_usec = 0;
+
+    setitimer(ITIMER_REAL, &tv, NULL);
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -124,11 +295,10 @@ int main(int argc, char **argv)
     //     exit(1);
     // }
 
-    
-
+    struct thread_list_s *new_thread;
     for (;;) {
         syslog(LOG_DEBUG, "Attempting to accept...");
-        remote_fd = accept(sockfd, &their_addr, &addr_size);
+        int remote_fd = accept(sockfd, &their_addr, &addr_size);
 
         if (remote_fd == -1) {
             perror("accept");
@@ -138,97 +308,42 @@ int main(int argc, char **argv)
         }
 
         printf("Recieved fd: %d\n", remote_fd);
-
         inet_ntop(AF_INET, &(((struct sockaddr_in*)&their_addr)->sin_addr), their_addr_str, INET_ADDRSTRLEN);
         syslog(LOG_DEBUG, "Accepting connection from %s", their_addr_str);
 
-        for (;;)
-        {
-            // Recieve data
-            char buf[MAX_DATA_SIZE];
-            int numbytes, packet_size = 0;
-            bool packet_recieved = false;
+        new_thread = malloc(sizeof(*new_thread));
+        if (new_thread == NULL) {
+            perror("malloc");
+            exit(__LINE__);
+        }
 
-            while (!packet_recieved) {
-                numbytes = recv(remote_fd, buf, MAX_DATA_SIZE - 1, 0);
-                if (numbytes == -1) {
-                    perror("recv");
-                    exit(__LINE__);
-                }
-                else {
-                    printf("Recieved %d bytes from %s\n", numbytes, their_addr_str);
-                }
+        new_thread->conn_thread_args.clientfd = remote_fd;
+        new_thread->conn_thread_args.status_flag = false;
 
-                if (numbytes == 0) {
-                    syslog(LOG_DEBUG, "Connection closed by remote");
-                    break;
-                }
+        // Create a new thread
+        if (pthread_create(&(new_thread->thread_id), NULL, process_connection, &(new_thread->conn_thread_args)) != 0) {
+            perror("pthread_create");
+            exit(__LINE__);
+        }
 
-                packet = realloc(packet, packet_size + numbytes + 1);
-                if (packet == NULL) {
-            fprintf(stderr, "realloc failed with errno: %d\n", errno); // Add this line
-                    perror("realloc");
-                    exit(__LINE__);
-                }
-
-                memcpy(packet + packet_size, buf, numbytes);
-
-                packet_size += numbytes;
-
-                // Check if packet is complete
-                for (char *c = packet; c != packet + packet_size; c++)
-                {
-                    if (*c == '\n') packet_recieved = true;
-                }
-            }
-
-            if (numbytes > 0) {
-                // Packet is recieved
-                // So dump
-                size_t bwritten = 0;
-                bwritten = fwrite(packet, sizeof(char), packet_size, fptr);
-                if (bwritten != packet_size) {
-                    perror("fwrite");
-                    exit(__LINE__);
-                }
-                fflush(fptr);
-            }
-
-            fseek(fptr2, 0, SEEK_SET);
-            // Transmit back
-            for (;numbytes > 0;)
+        SLIST_INSERT_HEAD(&head, new_thread, entries);
+        // Join threads that are completed
+        struct thread_list_s *tp;
+        SLIST_FOREACH_SAFE(new_thread, &head, entries, tp) {
+            // Join
+            if (new_thread->conn_thread_args.status_flag == true)
             {
-                syslog(LOG_DEBUG, "Transmitting back...");
-                size_t bread = fread(buf, sizeof(char), MAX_DATA_SIZE, fptr2);
-
-                // Transmit
-                if ((bread > 0) && (send(remote_fd, buf, bread, 0) == -1)) {
-                    perror("send");
+                printf("Joining thread %d\n", (int)(new_thread->thread_id));
+                if (pthread_join(new_thread->thread_id, NULL) != 0) {
+                    perror("pthread_join");
                     exit(__LINE__);
-                } else {
-                    printf("Sent %d data bytes to %s", bread, their_addr_str);
                 }
-
-                if (bread != MAX_DATA_SIZE) {
-                    syslog(LOG_DEBUG, "Transmission complete");
-                    break; // Tx complete
-                }
-            }
-
-            if (numbytes == 0) {
-                free(packet);
-                packet = NULL;
-                syslog(LOG_DEBUG, "Closed connection from %s", their_addr_str);
-                break;
+                // Remove 
+                SLIST_REMOVE(&head, new_thread, thread_list_s, entries);
+                free(new_thread);
             }
         }
     }
-    close(remote_fd);
-    close(sockfd);
-    fclose(fptr);
-    fclose(fptr2);
-
-    syslog(LOG_DEBUG, "Shouldn't reach here");
 
     return 0;
 }
